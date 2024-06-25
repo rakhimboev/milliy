9d9e84d565a24763ae8441e0fb0c03c4c868ff12
unknown <dilshodbekrakhimbaev@gmail.com>
2024-06-25 17:51:57 +0500

    feat: MongoDB connection and Express integration

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,16 @@
-console.log('Hello World!');
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import app from "./app";
 
+mongoose
+  .set("strictQuery", false)
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection successful");
+    const PORT = process.env.PORT || 3003;
+    app.listen(PORT, () => {
+        console.log(`The server is listening on ${PORT}`);
+      });
+  })
+  .catch((err) => console.log("Error connecting to MongoDB!", err));
